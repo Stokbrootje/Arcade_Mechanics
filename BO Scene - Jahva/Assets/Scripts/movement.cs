a77a1b2e324defa1151d8d7266f4fb94d6e21755
10KJVN <Jahva_game11@hotmail.com>
2022-03-30 19:01:29 +0200

    Unfinished health bar toegevoegd uit eigen scene

---
+++ BO Scene - Jahva/Assets/Scripts/movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool canJump = false;
+    public float speed = 10;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+
+        {
+            transform.position += -transform.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime;
+        }
+
+        if (canJump == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 6, 0);
+            canJump = false;
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        canJump = true;
+    }
+}
